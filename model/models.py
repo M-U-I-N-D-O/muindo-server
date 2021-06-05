8af5f8c14ee39c259558a84a2c6404b4942247b4
suram <swamys@naver.com>
2021-06-05 14:43:39 +0900

    feat : 룩 디비에 저장

---
+++ model/models.py
@@ -0,0 +1,85 @@
+# coding: utf-8
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+
+class Item(db.Model):
+    __tablename__ = 'item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    color = db.Column(db.String(45))
+    category = db.Column(db.String(45))
+    updated = db.Column(db.DateTime)
+    brand = db.Column(db.String(45))
+    subcategory = db.Column(db.String(45))
+    price = db.Column(db.Integer)
+    musinsa = db.Column(db.String(100))
+    itemno = db.Column(db.String(45))
+
+
+
+class Look(db.Model):
+    __tablename__ = 'look'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime)
+    userid = db.Column(db.ForeignKey('user.id'), index=True)
+    hat = db.Column(db.ForeignKey('item.id'), index=True)
+    top = db.Column(db.ForeignKey('item.id'), index=True)
+    bottom = db.Column(db.ForeignKey('item.id'), index=True)
+    shoes = db.Column(db.ForeignKey('item.id'), index=True)
+    bag = db.Column(db.ForeignKey('item.id'), index=True)
+    ok = db.Column(db.Integer)
+    no = db.Column(db.Integer)
+    url = db.Column(db.String(100))
+
+    item = db.relationship('Item', primaryjoin='Look.bag == Item.id', backref='item_item_item_item_looks0')
+    item1 = db.relationship('Item', primaryjoin='Look.bottom == Item.id', backref='item_item_item_item_looks_0')
+    item2 = db.relationship('Item', primaryjoin='Look.hat == Item.id', backref='item_item_item_item_looks1')
+    item3 = db.relationship('Item', primaryjoin='Look.shoes == Item.id', backref='item_item_item_item_looks_1')
+    item4 = db.relationship('Item', primaryjoin='Look.top == Item.id', backref='item_item_item_item_looks2')
+    user = db.relationship('User', primaryjoin='Look.userid == User.id', backref='looks')
+
+    def __init__(self, items:dict):
+
+        self.hat = items.get('hat')
+        self.top = items.get('top')
+        self.bottom=items.get('bottom')
+        self.bag=items.get('bag')
+        self.shoes=items.get('shoes')
+        self.url=items.get('url')
+        self.userid=items.get('userid')
+        self.created=datetime.now()
+
+
+class Style(db.Model):
+    __tablename__ = 'style'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(100))
+    gender = db.Column(db.Integer)
+    type = db.Column(db.String(45))
+    title = db.Column(db.String(45))
+    item_1 = db.Column(db.Integer)
+    item_2 = db.Column(db.Integer)
+    item_3 = db.Column(db.Integer)
+    item_4 = db.Column(db.Integer)
+    item_5 = db.Column(db.Integer)
+    item_6 = db.Column(db.Integer)
+    musinsa = db.Column(db.String(100))
+
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
+    email = db.Column(db.String(45))
+    oauth = db.Column(db.String(10))
+    uid = db.Column(db.String(45))
