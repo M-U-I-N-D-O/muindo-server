5936959237de0ca47e47dd2eb6cd028f15b08af2
sungheon jeong <elon4856@gmail.com>
2021-06-11 06:19:14 +0900

    feat:guest-auth

--- view/auth.py
+++ view/auth.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint,request
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt
 from flask_apispec import doc, use_kwargs, marshal_with
 from service.auth import *
 from serializers.auth import *
 from repository.auth import *
+
+import random
+import string
+
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -59,3 +63,24 @@ def refresh():
 def hello():
     print(get_jwt()['sub'])
     return "hello"
+
+
+
+@doc(tags=['auth'], description='로그인')
+@doc(params={
+        'Authorization': {
+            'description':
+            'Authorization HTTP header with JWT REFRESH token, like: Authorization: Bearer asdf.qwer.zxcv',
+            'in':'header',
+            'type':'string',
+            'required': True
+        }
+    })
+@auth.route('/access-token-guest', methods=['POST'])
+@marshal_with(TokensSchema, code=201)
+def get_access_token_for_guest():
+    guest = request.args
+    uid = "".join([random.choice(string.ascii_letters) for _ in range(10)])
+    if_first_time_insert_db("guset@guset.com", guest, "guest", uid)
+    user_id = get_user_id(uid)
+    return AuthService.create_tokens(user_id)
