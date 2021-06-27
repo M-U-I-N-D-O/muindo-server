a7dfc8dec9bf6dd3817fbd78e9df2690863f556b
Sungheon Jeong <elon4856@gmail.com>
2021-06-27 20:45:28 +0900

    FIX: guest 로그인 레이어 쪼개기 & 코드 간결화

--- repository/auth.py
+++ repository/auth.py
@@ -8,7 +8,6 @@ def db_check_if_not_insert_to_db_and_get_user_id(email, name, provider, unique_i
         user = User(name=name, email=email, uid=unique_id, oauth=provider)
         db.session.add(user)
         db.session.commit()
-        return user.id
     return user.id
 
 def guset_insert_to_db(email, name, provider, unique_id):
